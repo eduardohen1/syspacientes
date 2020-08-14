46430b86dc714af7c042cfed6a4bae38ee80e5b0
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-08-14 20:02:02 -0300

    Criado conexao com banco de dados

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-   echo "boa noite!";
+   //echo "boa noite!";
+   $con = mysqli_connect(
+      "localhost",
+      "root",
+      "123456",
+      "syspacientes");
+   if(!$con){
+      echo "Error ".PHP_EOL;
+      exit;
+   }
+   echo "Conectou!!!";
+   echo "<br>";
+   echo "Informações do host: " . 
+      mysqli_get_host_info($con);
+   mysqli_close($con);   
 ?>
