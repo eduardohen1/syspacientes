accd1fa80f0e07c8a722b83ff92ede36b6e4b7e4
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-08-20 21:16:30 -0300

    Criando $_GET

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
    //echo "boa noite!";
+   /*
    $con = mysqli_connect(
       "localhost",
       "root",
@@ -13,5 +14,9 @@
    echo "<br>";
    echo "Informações do host: " . 
       mysqli_get_host_info($con);
-   mysqli_close($con);   
+   mysqli_close($con);  
+   */
+   $nome = $_GET["nome"];
+   echo "O Nome é: ".$nome;
+   
 ?>
