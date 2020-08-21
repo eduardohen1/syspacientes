1083fe622bca0e6e629d9ab476be9923b95b3902
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-08-20 22:04:39 -0300

    Validação de senha e retorno

--- redirect.php
+++ redirect.php
@@ -1,15 +1,24 @@
 <?php
    $email = $_POST["inputEmail"];
    $senha = $_POST["inputPassword"];
+   $validou = true;
 
    //validar senha
    if(strlen($senha) < 6){
-      echo "Senha menor que 6 caracteres";
-   }else if(strlen($senha) > 7){
-      echo "Senha maior que 6 caracteres";
+      //echo "Senha menor que 6 caracteres";
+      $validou = false;
+   }else if(strlen($senha) > 6){
+      //echo "Senha maior que 6 caracteres";
+      $validou = false;
    }
-   echo "<hr>";
-   echo "E-mail: " . $email."<br>";
-   echo "Senha: ".$senha;
+
+   //exibir ou retornar:
+   if($validou){
+      echo "<hr>";
+      echo "E-mail: " . $email."<br>";
+      echo "Senha: ".$senha;
+   }else{
+      header("location:index.php");
+   }
 
 ?>
