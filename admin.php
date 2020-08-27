c2805c5de6d554a0b28fa4e36164ee01c3a4b892
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-08-27 20:53:00 -0300

    .

--- admin.php
+++ admin.php
@@ -82,7 +82,7 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+    <script>window.jQuery || document.write('<script src="https://code.jquery.com/jquery-1.12.4.min.js"><\/script>')</script>
     <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
