06eed4388958185dd99d4788c9a569da49c8f99d
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-09-03 14:16:57 -0300

    Usuario_gravar

--- usuario_gravar.php
+++ usuario_gravar.php
@@ -3,15 +3,11 @@
    require_once('variaveis.php');
    require_once('conexao.php');
 
-   echo("oi");
-
    $senha      = $_POST["inputPassword"];
    $email      = $_POST["inputEmail"];
    $nome       = $_POST["inputNome"];
    $id_usuario = $_POST["inputIdUsuario"];
    
-   echo("$senha | $email | $nome | $id_usuario");
-   exit();
    if(strlen($id_usuario) > 0){
       //atualizar
       $sql = "UPDATE usuario SET nome = '$nome', email='$email', senha='$senha' WHERE id_usuario = $id_usuario";
