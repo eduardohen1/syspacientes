7ead80eb6e4cb8abd0f4aa8c83c10c80b2327d33
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-09-03 21:44:46 -0300

    Ajustando layout de usuario

--- usuario.php
+++ usuario.php
@@ -29,34 +29,74 @@
    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-   <h1>Editando o usuário <?php echo($nomeUsuario); ?></h1>
-   <hr>
    <div class="container">
-   <form
-      method="post"
-      action="usuario_gravar.php">
-      <div class="form-group">
-         <label for="inputNome">Nome do usuário:</label>
-         <input type="text" class="form-control" id="inputNome" 
-                name="inputNome" placeholder="Nome do usuário"
-                value="<?php echo($nomeUsuario); ?>"
-                >
+      <!-- Static navbar -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+        <a class="navbar-brand" href="#">SysPacientes</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample09">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="admin.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown active">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown09" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cadastros</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown09">
+                <a class="dropdown-item" href="#">Cadastro de pessoas</a>
+                <a class="dropdown-item" href="usuario_list2.php">Cadastro de usuários</a>                
+                <a class="dropdown-item" href="#">Cadastro de pacientes</a>
+              </div>
+            </li>
+          </ul>  
+          <ul class="navbar-nav navbar-right">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown10" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <?php echo($nome_usuario); ?>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="dropdown10">
+                <a class="dropdown-item" href="minhaconta.php">Minha conta</a>
+                <a class="dropdown-item" href="logout.php">Sair</a>                 
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Editando o usuário: <?php echo($nomeUsuario); ?></h1>
+        <form
+            method="post"
+            action="usuario_gravar.php">
+            <div class="form-group">
+               <label for="inputNome">Nome do usuário:</label>
+               <input type="text" class="form-control" id="inputNome" 
+                     name="inputNome" placeholder="Nome do usuário"
+                     value="<?php echo($nomeUsuario); ?>"
+                     >
+            </div>
+            <div class="form-group">
+               <label for="inputEmail">E-mail:</label>
+               <input type="email" class="form-control" id="inputEmail" 
+                     name="inputEmail" placeholder="E-mail"
+                     value="<?php echo($emailUsuario); ?>"
+                     >
+            </div>
+            <div class="form-group">
+               <label for="inputPassword">Senha</label>
+               <input type="text" class="form-control" id="inputPassword" name="inputPassword" value="<?php echo($senhaUsuario);?>">
+            </div>
+            <input type="hidden" id="inputIdUsuario" name="inputIdUsuario" value="<?php echo($idUsuario) ?>">
+            <button type="submit" class="btn btn-success">Gravar</button>&nbsp;
+            <a href="usuario_list2.php" class="btn btn-warning" role="button">Retornar</a>
+         </form>
       </div>
-      <div class="form-group">
-         <label for="inputEmail">E-mail:</label>
-         <input type="email" class="form-control" id="inputEmail" 
-                name="inputEmail" placeholder="E-mail"
-                value="<?php echo($emailUsuario); ?>"
-                >
-      </div>
-      <div class="form-group">
-         <label for="inputPassword">Senha</label>
-         <input type="text" class="form-control" id="inputPassword" name="inputPassword" value="<?php echo($senhaUsuario);?>">
-      </div>
-      <input type="hidden" id="inputIdUsuario" name="inputIdUsuario" value="<?php echo($idUsuario) ?>">
-      <button type="submit" class="btn btn-success">Gravar</button>&nbsp;
-      <a href="usuario_list.php" class="btn btn-warning" role="button">Retornar</a>
-   </form>
-   </div>
+    </div>
+
+
+
 </body>
 </html>
