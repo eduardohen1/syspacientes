7e8169b64735dd5a6f3804e703ad5f6be30dafa8
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-09-03 22:15:25 -0300

    tratamento para não excluir usuario logado

--- usuario_list2.php
+++ usuario_list2.php
@@ -101,7 +101,8 @@
                      echo("<td>$email</td>");
                      echo("<td>");
                         echo("<a class='btn btn-lg btn-success' href='usuario.php?idUsuario=$id' role='button'>Editar</a>&nbsp;");
-                        echo("<a class='btn btn-lg btn-danger'  href='usuario_excluir.php?idUsuario=$id' role='button'>Excluir</a>");
+                        if($id != $id_usuario)
+                          echo("<a class='btn btn-lg btn-danger'  href='usuario_excluir.php?idUsuario=$id' role='button'>Excluir</a>");
                      echo("</td>");
                      echo("</tr>");
                   } 
