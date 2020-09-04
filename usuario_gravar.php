a845c9b3f8ca32814f4870872cdfd6984911676a
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-09-04 19:59:32 -0300

    Ajuste na query de INSERT e UPDATE

--- usuario_gravar.php
+++ usuario_gravar.php
@@ -3,18 +3,25 @@
    require_once('variaveis.php');
    require_once('conexao.php');
 
-   $senha      = $_POST["inputPassword"];
-   $email      = $_POST["inputEmail"];
-   $nome       = $_POST["inputNome"];
-   $id_usuario = $_POST["inputIdUsuario"];
+   $senha       = $_POST["inputPassword"];
+   $email       = $_POST["inputEmail"];
+   $nome        = $_POST["inputNome"];
+   $tipo_acesso = $_POST["lstTipoAcesso"];
+   $id_usuario  = $_POST["inputIdUsuario"];
    
    if(strlen($id_usuario) > 0){
       if($id_usuario != 0){
          //atualizar
-         $sql = "UPDATE usuarios SET nome='$nome', email='$email', senha='$senha' WHERE id = $id_usuario";
+         $sql = "UPDATE usuarios SET 
+                  nome='$nome', 
+                  email='$email', 
+                  senha='$senha',
+                  tipo_acesso= $tipo_acesso
+                 WHERE id = $id_usuario";
       }else{
          //insert
-         $sql = "INSERT INTO usuarios(nome, email, senha)VALUES('$nome', '$email', '$senha')";
+         $sql = "INSERT INTO usuarios(   nome,    email,    senha,tipo_acesso)
+                               VALUES('$nome', '$email', '$senha',$tipo_acesso)";
       }
       mysqli_query($conexao_bd, $sql);
    }else{
