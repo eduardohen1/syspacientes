1256983c088bd1ad4154705aa7438d6b8bbc72ae
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2020-09-04 20:35:06 -0300

    Acrescentando SweetAlert

--- usuario_list2.php
+++ usuario_list2.php
@@ -28,8 +28,11 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>SysPacientes - Lista de usuários</title>
     <link rel="icon" href="img/favicon/favicon2.ico">
+    <script src="js/jquery.js"></script>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/navbar.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/sweetalert2.css">
+    <script src="js/sweetalert2.js"></script>
    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
@@ -137,10 +140,26 @@
     ?>
     <script type="text/javascript">
       function excluirUsuario(id){
+        /*
         var resp = confirm('Deseja realmente excluir este usuário?');
         if(resp == true){
           window.location.href = "usuario_excluir.php?idUsuario=" + id;
         }
+        */
+        Swal.fire({
+          title: 'Deseja realmente exluir?',
+          text: "Você deseja realmente excluir este usuário!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'SIM'
+          cancelButtonText: 'NÃO'
+        }).then((result) => {
+          if (result.value) {
+            window.location.href = "usuario_excluir.php?idUsuario=" + id;
+          }
+        });
       }
     </script>
 </body>
